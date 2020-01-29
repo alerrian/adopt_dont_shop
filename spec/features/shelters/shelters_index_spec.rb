9a06fa05c21a3dc30b75139a81cd5b52f69c552a
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-01-28 17:23:09 -0700

    Add test to find a link on index

--- spec/features/shelters/shelters_index_spec.rb
+++ spec/features/shelters/shelters_index_spec.rb
@@ -11,5 +11,11 @@ RSpec.describe 'shelters index page', type: :feature do
       expect(page).to have_content(shelter1.name)
       expect(page).to have_content(shelter2.name)
     end
+
+    it 'has a link to create a new shelter' do
+      visit '/shelters'
+
+      expect(page).to have_link('Create a new Shelter', href: '/shelters/create')
+    end
   end
 end
--- spec/features/shelters/shelters_new_spec.rb
+++ spec/features/shelters/shelters_new_spec.rb
@@ -1,21 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'shelters show page', type: :feature do
+RSpec.describe 'new shelter creation' do
   context 'as a user' do
-    it 'can see all data pertaining to shelters' do
-      shelter1 = Shelter.create(name: "Mike's Shelter",
-                                address: '1331 17th Street',
-                                city: 'Denver',
-                                state: 'CO',
-                                zip: '80202')
-
-      visit "/shelters/#{shelter1.id}"
-
-      expect(page).to have_content(shelter1.name)
-      expect(page).to have_content(shelter1.address)
-      expect(page).to have_content(shelter1.city)
-      expect(page).to have_content(shelter1.state)
-      expect(page).to have_content(shelter1.zip)
-    end
   end
 end
