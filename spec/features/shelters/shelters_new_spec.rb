6e31b0b4893ea429258e7297b5f16d0e0270a731
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-01-29 08:12:37 -0700

    Fix test for better coverage and change corresponding method to relate

--- spec/features/shelters/shelters_new_spec.rb
+++ spec/features/shelters/shelters_new_spec.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'new shelter creation' do
   context 'as a user' do
-    scenario 'can create a new shelter' do
+    it 'can create a new shelter' do
       visit '/shelters/new'
-      within('form') do
-        fill_in 'shelter[name]', with: 'New Shelter Name'
-        fill_in 'shelter[address]', with: '999 E. Road'
-        fill_in 'shelter[city]', with: 'City Name'
-        fill_in 'shelter[state]', with: 'State Abbrev'
-        fill_in 'shelter[zip]', with: 'Zip Code'
-      end
 
-      click_on 'submit'
-      expect(page).to have_content 'New Shelter Name'
+      fill_in 'shelter[name]', with: 'New Shelter Name'
+      fill_in 'shelter[address]', with: '999 E. Road'
+      fill_in 'shelter[city]', with: 'City Name'
+      fill_in 'shelter[state]', with: 'State Abbrev'
+      fill_in 'shelter[zip]', with: 'Zip Code'
+
+      click_button('shelter_submit')
+
+      expect(current_path).to eq('/shelters')
+      expect(page).to have_content('New Shelter Name')
     end
   end
 end
