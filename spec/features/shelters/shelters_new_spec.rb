543eb8901ac67dcda5877e84f22cf5a0bddcee5a
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-01-29 06:29:06 -0700

    Add new test for creating new shelter

--- spec/features/shelters/shelters_new_spec.rb
+++ spec/features/shelters/shelters_new_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'new shelter creation' do
   context 'as a user' do
-    
+    scenario 'can create a new shelter' do
+      visit '/shelters/new'
+      within('form') do
+        fill_in 'shelter[name]', with: 'New Shelter Name'
+        fill_in 'shelter[address]', with: '999 E. Road'
+        fill_in 'shelter[city]', with: 'City Name'
+        fill_in 'shelter[state]', with: 'State Abbrev'
+        fill_in 'shelter[zip]', with: 'Zip Code'
+      end
+
+      click_on 'submit'
+      expect(page).to have_content 'New Shelter Name'
+    end
   end
 end
